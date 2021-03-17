19cbb69fa0985ef4747755558198ac3e55c1b422
Mikey <mike.greys11@gmail.com>
2021-03-17 10:42:27 -0700

    Added image folder and title and icon section.

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ pygame.init()
 
 #Screen creation.
 screen = pygame.display.set_mode((800, 600))
+
+#Title and Icon.
+pygame.display.set_caption("Negative Space")
+icon = pygame.image.load('./images/ufo.png')
+pygame.display.set_icon(icon)
+
+#Game loop. This portion makes it so that the window stays open until we click the close (x) button.
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
