6ad8cf1dc0bcc8bca8d3a363112a11cd970a68af
Mikey <mike.greys11@gmail.com>
2021-03-18 15:40:13 -0700

    Added img to images, created player function and placed it on screen.

--- main.py
+++ main.py
@@ -11,13 +11,32 @@ pygame.display.set_caption("Negative Space")
 icon = pygame.image.load('./images/ufo.png')
 pygame.display.set_icon(icon)
 
+#Player
+playerImg = pygame.image.load("./images/player.png")
+playerX = 370
+playerY = 480
+
+
+#This draws player onto the screen. First argument is the image itself, while the second argument is the coordinates.
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
 #Game loop. This portion makes it so that the window stays open until we click the close (x) button.
 running = True
 while running:
+
+    #RGB values fill the screen.
+    screen.fill((0, 0, 128))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    #RGB values fill the screen.
-    screen.fill((0, 0, 128))
+
+
+    #Make sure that the player is called after the screen is created, not before.
+    player()
+
+
+    #As it says this updates the screen.
     pygame.display.update()
