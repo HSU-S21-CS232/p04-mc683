02c4b194945d402cf500eec95d9fdf536e6aefc3
Mikey <shpoopdy@gmail.com>
2021-03-31 11:38:25 -0700

    Created custom theme for the menu.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import pygame_menu
+from pygame_menu import Theme
 import math
 import random
 from pygame import mixer
@@ -7,6 +8,11 @@ from game import Game
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
+
+backgroundImg = pygame_menu.baseimage.BaseImage('./images/background.jpg')
+
+myTheme = Theme(background_color = (0, 0, 0, 0), title_background_color = (4, 47, 126), title_font_shadow = True, widget_padding = 25)
+myTheme.background_color = backgroundImg
 
 #background Sound. Music: “Star Way”, from PlayOnLoop.com Licensed under Creative Commons by Attribution 4.0
 mixer.music.load('./music/background.wav')
@@ -21,7 +27,7 @@ def start_game():
 
 
 # Menu stuff from pygame-menu.readthedocs.io/en/4.0.1/
-menu = pygame_menu.Menu(300, 400, 'Howdy', theme = pygame_menu.themes.THEME_BLUE)
+menu = pygame_menu.Menu(500, 500, 'Howdy', theme = myTheme)
 menu.add.text_input('Name:', default = 'Mikey')
 #menu.add.selector('Difficulty:', [('Hard', 1), ('Easy', 2)], onchange = set_difficulty)
 menu.add.button('Play', start_game)
