063837c504535cb60f0cb069f3a75faa36cac4c7
Mikey <shpoopdy@gmail.com>
2021-03-31 11:01:51 -0700

    Added music to menu.

--- main.py
+++ main.py
@@ -8,16 +8,22 @@ from game import Game
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
+#background Sound. Music: “Star Way”, from PlayOnLoop.com Licensed under Creative Commons by Attribution 4.0
+mixer.music.load('./music/background.wav')
+mixer.music.set_volume(0.1)
+mixer.music.play(-1)
+
 def set_difficulty():
     pass
 
 def start_game():
     Game()
 
+
 # Menu stuff from pygame-menu.readthedocs.io/en/4.0.1/
 menu = pygame_menu.Menu(300, 400, 'Howdy', theme = pygame_menu.themes.THEME_BLUE)
 menu.add.text_input('Name:', default = 'Mikey')
-menu.add.selector('Difficulty:', [('Hard', 1), ('Easy', 2)], onchange = set_difficulty)
+#menu.add.selector('Difficulty:', [('Hard', 1), ('Easy', 2)], onchange = set_difficulty)
 menu.add.button('Play', start_game)
 menu.add.button('Quit', pygame_menu.events.EXIT)
 
