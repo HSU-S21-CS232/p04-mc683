ffacba14417bc5f752e979b83a2ba5cda46e4c2b
Mikey <shpoopdy@gmail.com>
2021-03-30 17:01:52 -0700

    Added main.py that holds menu.

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+import pygame_menu
+import math
+import random
+from pygame import mixer
+from game import Game
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+
+def set_difficulty():
+    pass
+
+def start_game():
+    Game()
+
+
+menu = pygame_menu.Menu(300, 400, 'Howdy', theme = pygame_menu.themes.THEME_BLUE)
+menu.add.text_input('Name:', default = 'Mikey')
+menu.add.selector('Difficulty:', [('Hard', 1), ('Easy', 2)], onchange = set_difficulty)
+menu.add.button('Play', start_game)
+menu.add.button('Quit', pygame_menu.events.EXIT)
+
+menu.mainloop(screen)
